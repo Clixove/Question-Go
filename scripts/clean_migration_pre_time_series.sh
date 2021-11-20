1fdff2e5c2e7326d63c0e451d181719dbca1998f
cloudy-sfu <cloudy_stats@outlook.com>
2021-11-20 19:05:26 +0800

    Delete scripts directory

--- scripts/clean_migration_pre_time_series.sh
+++
@@ -1,8 +0,0 @@
-cd ~/question_go_v2/
-conda activate webapp
-python manage.py makemigrations
-python manage.py migrate --fake pre_time_series zero
-rm -r pre_time_series/migrations/__pycache__/
-rm pre_time_series/migrations/0*
-python manage.py makemigrations
-python manage.py migrate --fake-initial
--- scripts/migrate.sh
+++
@@ -1,5 +0,0 @@
-# shellcheck disable=SC2164
-cd ~/question_go_v2/
-conda activate webapp
-python manage.py makemigrations
-python manage.py migrate
